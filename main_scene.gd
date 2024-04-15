329c467742c374c64880aca688c460dc5c2522cb
Victor Reynolds <VictorReynolds@users.noreply.github.com>
2024-04-14 18:01:07 -0700

    Kitchen implementation

--- main_scene.gd
+++ main_scene.gd
@@ -1,7 +1,32 @@
 extends Node2D
 
+var wait_before_start = 5 # How logn to wait before starting the game
+var wait_before_summon = 5 # How long to wait before summon occurs
 
+var fridge_label = null
+var stove_label = null
+var fridge = null
+var stove = null
 
-func _on_summons_button_pressed():
-	get_tree().change_scene_to_file("res://dungeon.tscn")
+func _ready():
+	fridge_label = find_child("FridgeLabel")
+	fridge = get_node("Entities/Fridge")
 	
+	stove_label = find_child("StoveLabel")
+	stove = get_node("Entities/Stove")
+	
+	await get_tree().create_timer(wait_before_start).timeout
+	fridge_label.visible = true
+	fridge.enable_interaction()
+
+func _on_fridge_interacted():
+	fridge_label.visible = false
+	stove_label.visible = true
+	stove.enable_interaction()
+
+func _on_stove_interacted():
+	stove_label.visible = false
+	stove.find_child("EggSizzle").playing = true
+	await get_tree().create_timer(wait_before_summon).timeout
+	# START THE GAME BATTLE!!!
+	get_tree().change_scene_to_file("res://dungeon.tscn")
